8aa482835fe6abc114674909c4ec8d026aadf1bc
ShubhamPandey57 <shubhampandey2869@gmail.com>
2026-03-23 23:27:47 +0530

    updated midd

--- roleAuth/middleware/rolemid.js
+++ roleAuth/middleware/rolemid.js
@@ -1,6 +1,6 @@
 const jwt =require("jsonwebtoken")
 const authMiddleware=(req,res,next)=>{
-    let token=req.cookies.token
+    let token=req.session.token
     //console.log(token)
     if (!token && req.headers.authorization){
         token =req.headers.authorization.split(" ")[1]
@@ -8,6 +8,9 @@ const authMiddleware=(req,res,next)=>{
     if(!token){
         return res.json({message:"Not Logged In"})
     }
+
+
+    
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET)
         req.userId=decoded.id
