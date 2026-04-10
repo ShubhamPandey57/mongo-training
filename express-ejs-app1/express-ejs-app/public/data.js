51578dccc1cb2f57003fbc429e71da44afbb56db
ShubhamPandey57 <shubhampandey2869@gmail.com>
2026-04-10 12:44:42 +0530

    add new folder of car sevice

---
+++ express-ejs-app1/express-ejs-app/public/data.js
@@ -0,0 +1,101 @@
+const about=[
+    {
+        title:"Quality Servicing",
+        description:"very gudd",
+        link:"read more"
+    },
+    {
+        title:"Expert worker",
+        description:"very gudd",
+        link:"read more"
+    },
+    {
+        title:"modern Equipment",
+        description:"very gudd",
+        link:"read more"
+    },
+        {
+        title:"faster solutions",
+        description:"very gudd",
+        link:"read more"
+    }
+]
+
+const services=[
+    {
+        title:"Diagnostic Test",
+        image:"images/images/service-2.jpg",
+        price: 55.00,
+        duration: "45m"
+    },
+    {
+        title:"Engine Servicing",
+        image:"images/images/service-1.jpg",
+        price: 145.00,
+        duration: "2h"
+    },
+    {
+        title:"Tyre Replacement",
+        image:"images/images/service-5.jpg",
+        price: 120.00,
+        duration: "1h"
+    },
+    {
+        title:"Oil Change",
+        image:"images/images/service-3.jpg",
+        price: 42.00,
+        duration: "30m"
+    }
+]
+
+const team=[
+      {
+        name:"Harshit",
+        role:"CEO",
+        image:"images/images/p1.jpg"
+      },
+      {
+        name:"Siri",
+        role:"Chief Executive",
+        image:"images/images/p2.jpg"
+      },
+      {
+        name:"Max",
+        role:"Mechanic",
+        image:"images/images/p3.jpg"
+      },
+      {
+        name:"Rehan",
+        role:"technician",
+        image:"images/images/p4.jpg"
+      },
+      {
+        name:"rashmika",
+        role:"helper",
+        image:"/images/helper.jpg"
+      }
+]
+
+const clients=[
+    {
+        name:"neeru Bajva",
+        review:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora iste consequuntur odio!",
+        image:"/images/custumer1.jpg"
+    },
+    {
+        name:"madison beer",
+        review:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora iste consequuntur odio!",
+        image:"/images/custumer2.jpg"
+    },
+    {
+        name:"megan fox",
+        review:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora iste consequuntur odio!",
+        image:"/images/custumer3.jpg"
+    },
+    {
+        name:"rupali",
+        review:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora iste consequuntur odio!",
+        image:"/images/custumer4.jpg"
+    }
+]
+module.exports={about,services,team,clients}
